57d944754d9918c87c66a9f12c636c998919e369
DarkestDragon <53903518+DarkestDragon@users.noreply.github.com>
2019-10-27 17:17:01 +0300

    Release version

---
+++ office_feedback.py
@@ -0,0 +1,21 @@
+#! python3.7
+
+from dialog_bot_sdk.bot import DialogBot
+import grpc
+
+def on_msg(*params):
+	global bot_owner
+	if params[0].message.textMessage.text == "/start" and bot_owner == None:
+		bot_owner = [[params[0].peer, params[0].sender_uid]]
+	elif params[0].message.textMessage.text != "" and bot_owner[0] != [params[0].peer, params[0].sender_uid]:
+		bot.messaging.reply(bot_owner[0][0], [params[0].mid])
+		bot.messaging.send_message(params[0].peer, "Благодарю за отзыв!")
+
+if __name__ == "__main__":
+	bot = DialogBot.get_secure_bot(
+		"hackathon-mob.transmit.im",
+		grpc.ssl_channel_credentials(),
+		"14e01ad53f5f39660b3678b569a204f38fea7607"
+	)
+	bot_owner = None #stores peer and id of bot owner
+	bot.messaging.on_message(on_msg)
